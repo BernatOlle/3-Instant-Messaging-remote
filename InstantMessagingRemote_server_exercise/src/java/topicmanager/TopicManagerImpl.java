fc059987c8fb7219c5f6472350f9b79350c05ba0
BernatOlle <bernatollef@gmail.com>
2024-11-28 17:46:17 +0100

    Previous exercice implementation

--- InstantMessagingRemote_server_exercise/src/java/topicmanager/TopicManagerImpl.java
+++ InstantMessagingRemote_server_exercise/src/java/topicmanager/TopicManagerImpl.java
@@ -19,34 +19,61 @@ public class TopicManagerImpl implements TopicManager {
     topicMap = new HashMap<Topic, Publisher>();
   }
 
-  @Override
+   @Override
   public Publisher addPublisherToTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      if (topicMap.containsKey(topic)) {
+        Publisher act_publisher = topicMap.get(topic);
+        act_publisher.incPublishers();
+        return topicMap.get(topic); // Return the existing publisher
+    }
+    
+    Publisher publisher = new PublisherImpl(topic);
+    topicMap.put(topic, publisher);
+    return publisher;
   }
 
   @Override
   public void removePublisherFromTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if (topicMap.containsKey(topic)) {
+           topicMap.remove(topic);   
+    }
   }
 
   @Override
   public Topic_check isTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    boolean check = topicMap.containsKey(topic);
+    return new Topic_check(topic,check);
   }
 
   @Override
   public List<Topic> topics() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return new ArrayList<Topic>(topicMap.keySet());
   }
 
   @Override
   public Subscription_check subscribe(Topic topic, Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    if (!topicMap.containsKey(topic)) {
+        return new Subscription_check(topic, Subscription_check.Result.NO_TOPIC);
+    }
+    Publisher publisher = topicMap.get(topic);
+    if (publisher == null) {
+        return new Subscription_check(topic, Subscription_check.Result.NO_SUBSCRIPTION);
+    }
+    publisher.attachSubscriber(subscriber);
+    return new Subscription_check(topic, Subscription_check.Result.OKAY);
   }
 
   @Override
   public Subscription_check unsubscribe(Topic topic, Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    if (!topicMap.containsKey(topic)) {
+        return new Subscription_check(topic, Subscription_check.Result.NO_TOPIC);
+    }
+    Publisher publisher = topicMap.get(topic);
+    if (publisher == null) {
+        return new Subscription_check(topic, Subscription_check.Result.NO_SUBSCRIPTION);
+    }
+    publisher.detachSubscriber(subscriber);
+    return new Subscription_check(topic, Subscription_check.Result.OKAY);
   }
   
   public Publisher publisher(Topic topic){
