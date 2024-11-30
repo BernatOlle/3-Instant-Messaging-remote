a790455be4afec654f7b3cf1ceed4d6eaf8b53b5
JaumeMLl <jaume.motje@estudiantat.upc.edu>
2024-11-30 01:53:59 +0100

    Wew Service Development
    Missing WebSocket Implementation

--- InstantMessagingRemote_client_exercise/src/topicmanager/TopicManagerStub.java
+++ InstantMessagingRemote_client_exercise/src/topicmanager/TopicManagerStub.java
@@ -25,32 +25,38 @@ public class TopicManagerStub implements TopicManager {
 
   @Override
   public Publisher addPublisherToTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    apiREST_TopicManager.addPublisherToTopic(topic);
+    return new PublisherStub(topic);
   }
 
   @Override
   public void removePublisherFromTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    apiREST_TopicManager.removePublisherFromTopic(topic);
   }
 
   @Override
   public Topic_check isTopic(Topic topic) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return apiREST_TopicManager.isTopic(topic);
   }
 
   @Override
   public List<Topic> topics() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return apiREST_TopicManager.topics();
   }
 
   @Override
   public Subscription_check subscribe(Topic topic, Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    // TODO: Calls WebSocket service to handle subscription
+    
+    // Implement subscription logic, like sending a subscription request to the server
+    // For simplicity, we're returning a "mocked" subscription check here.
+    return new Subscription_check(topic, Subscription_check.Result.OKAY);  // Mocked response
   }
 
   @Override
   public Subscription_check unsubscribe(Topic topic, Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    // TODO: Calls WebSocket service to handle unsubscription
+    // Similar to subscribe, we're returning a mocked response for unsubscription
+    return new Subscription_check(topic, Subscription_check.Result.OKAY);  // Mocked response
   }
-
 }
